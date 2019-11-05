58e76df1335acfcf72fb0962f892950e617bcdbd
gBabo <goncalo.b.freire@tecnico.ulisboa.pt>
2019-11-05 15:09:02 +0000

    nao adiciono x2

--- projeto3/js/spotlight.js
+++ projeto3/js/spotlight.js
@@ -7,12 +7,12 @@ class Spotlight extends THREE.Object3D{
     this.light = new THREE.SpotLight(0xfffffff, 1, 250, Math.PI/8, 0, 1);
 
     this.add(this.light);
-    this.add(this.light.target);
+    //this.add(this.light.target);
 
     this.light.position.set(pos[0], pos[1], pos[2]);
     this.light.target.position.set(target[0], target[1], target[2]);
 
-    this.updateMatrixWorld();
+    this.light.target.updateMatrixWorld();
     //this.helper = new THREE.SpotLightHelper(this.light);
     //this.add(this.helper);
   }
